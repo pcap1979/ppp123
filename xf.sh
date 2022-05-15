0b33ead77ec67096388fbe8e4a220a768c0a26c0
pcap1979 <48285370+pcap1979@users.noreply.github.com>
2022-05-15 20:52:41 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "ab15c71a-d130-45ce-a7e6-d0408b84119e",
                         "alterId": 0
                     }
                 ],
